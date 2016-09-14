a5bb8cee8badca5a9b3b1c9f79f4ec4864e6be6c
Jared Mahabali <jmahabali@partners.org>
2016-09-14 14:54:14 -0400

    Change units

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -9,16 +9,17 @@ class DictionaryTest < Minitest::Test
   # iterators that can be used to write custom tests
   include Spout::Helpers::Iterators
 
-  VALID_UNITS = ['', 'naps', 'days', 'nights', 'events per hour', 'event count', 'minutes', 'hours',
-   'beats per minute', 'seconds', 'percent', 'good days', 'weekdays', 'weekend days', 'valid nights', 'invalid nights' , 'wake bouts' , 'sleep bouts' ]
+  VALID_UNITS = [nil, '', 'naps', 'days', 'nights', 'events per hour', 'event count', 'minutes', 'hours',
+   'beats per minute', 'seconds', 'percent', 'good days', 'weekdays', 'weekend days', 'valid nights', 'invalid nights' , 'wake bouts' , 'sleep bouts' , 'activity counts' , 'non-workdays' ,
+    'naps per day' , 'minutes per day' , 'reliable days' , 'workdays']
 
-  @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
-    define_method("test_units: "+variable.path) do
-      message = "\"#{variable.units}\"".colorize( :red ) + " invalid units.\n" +
-                "             Valid types: " +
-                VALID_UNITS.sort.collect{|u| u.inspect.colorize( :white )}.join(', ')
-      assert VALID_UNITS.include?(variable.units), message
-    end
-  end
+   @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
+     define_method("test_units: "+variable.path) do
+       message = "\"#{variable.units}\"".colorize( :red ) + " invalid units.\n" +
+                 "             Valid types: " +
+                 VALID_UNITS.sort_by(&:to_s).collect{|u| u.inspect.colorize( :white )}.join(', ')
+       assert VALID_UNITS.include?(variable.units), message
+     end
+   end
 
 end
